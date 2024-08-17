122aabbee64d1474270e861011449ea9a18fe1a9
Vaibhavsun <125079612+Vaibhavsun@users.noreply.github.com>
2024-08-17 18:58:33 +0530

    Update settings.py

--- hospital_login/settings.py
+++ hospital_login/settings.py
@@ -58,7 +58,7 @@ MEDIA_URL='/media/'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
