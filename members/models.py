ebbb0edb5e14255500fa5dc01a2b9d9d0ef9afae
Vaibhavsun <125079612+Vaibhavsun@users.noreply.github.com>
2024-08-17 19:24:49 +0530

    Update models.py

--- members/models.py
+++ members/models.py
@@ -3,7 +3,7 @@ class main_model(models.Model):
     user=models.CharField(default="patients",max_length=50)
     first_name=models.CharField(max_length=100)
     last_name=models.CharField(max_length=100)
-    profile_pit=models.ImageField(upload_to='profile_pic')
+    profile_pit=models.ImageField(upload_to='profile_pic',blank=True)
     username=models.CharField(max_length=100)
     email=models.EmailField()
     password=models.CharField(max_length=100)
